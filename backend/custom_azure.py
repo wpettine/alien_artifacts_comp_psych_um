f26c699efc245c4ce381a547150319210f8e634d
Warren Woodrich Pettine <warren.pettine@gmail.com>
2022-10-13 14:20:10 -0700

    fixed header for the storage account

--- backend/custom_azure.py
+++ backend/custom_azure.py
@@ -1,13 +1,13 @@
 from storages.backends.azure_storage import AzureStorage
 
 class AzureMediaStorage(AzureStorage):
-    account_name = 'alienartifactsstorage' # Must be replaced by your <storage_account_name>
+    account_name = 'umalienartifactsstorage' # Must be replaced by your <storage_account_name>
     account_key = 'T3fyAzfvTEK7jwh8h5gKYudcJhjFvTgnlYXMY6+aSx0A8bNuBckt0xYB7ydaLcSO1lL5nVEkq1Nv+ASt37H7fQ==' # Must be replaced by your <storage_account_key>
     azure_container = 'media'
     expiration_secs = None
 
 class AzureStaticStorage(AzureStorage):
-    account_name = 'alienartifactsstorage' # Must be replaced by your storage_account_name
+    account_name = 'umalienartifactsstorage' # Must be replaced by your storage_account_name
     account_key = 'T3fyAzfvTEK7jwh8h5gKYudcJhjFvTgnlYXMY6+aSx0A8bNuBckt0xYB7ydaLcSO1lL5nVEkq1Nv+ASt37H7fQ==' # Must be replaced by your <storage_account_key>
     azure_container = 'static'
     expiration_secs = None
