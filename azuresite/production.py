0cab7ede27a7095ff9d4c6bcec88f0f2022dc25b
Warren Woodrich Pettine <warren.pettine@gmail.com>
2022-10-17 12:42:05 -0700

    changed username

--- azuresite/production.py
+++ azuresite/production.py
@@ -42,7 +42,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ['DBNAME'],
         'HOST': hostname + ".postgres.database.azure.com",
-        'USER': os.environ['DBUSER'] + "@" + hostname,
+        # 'USER': os.environ['DBUSER'] + "@" + hostname,
+        'USER': os.environ['DBUSER'],
         'PASSWORD': os.environ['DBPASS'] 
     }
 }
