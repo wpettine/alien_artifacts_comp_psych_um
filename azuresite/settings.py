895e13c8f9a6729fa0e9103523ce6fede9104cde
Michael Griffin <mike@munchdevelopment.com>
2022-10-18 16:18:12 -0500

    update ALLOWED_HOSTS

--- azuresite/settings.py
+++ azuresite/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'temp-secret-key')
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 # DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 # Application definition
 
